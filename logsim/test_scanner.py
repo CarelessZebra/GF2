d3424fe6acb4c40e43931bdf4f40ea51418be492
Adi Jain <adi.jain1357@gmail.com>
2025-05-26 10:59:05 +0100

    Testing for scannner

--- logsim/test_scanner.py
+++ logsim/test_scanner.py
@@ -8,3 +8,13 @@ def scanner():
     """Fixture to create a Scanner instance"""
     return Scanner()
 
+@pytest.fixture
+def scanner_file():
+    return "logsim/test_full_adder.txt"
+
+@pytest.fixture
+def scanner_with_file(scanner_file):
+    """Fixture to create a Scanner instance with a file"""
+    return Scanner(scanner_file)
+
+
